c22baf6d8fdf662c3d06ebcf1fda72434e4320c7
rupali dudhal <rupalidudhal95@gmail.com>
2025-11-24 00:36:24 +0530

    stopwatch is created

--- stopwatch/src/StopWatch.jsx
+++ stopwatch/src/StopWatch.jsx
@@ -1,42 +1,42 @@
 import { useEffect, useState } from "react";
-export default function StopWatch() {
-  const [toggle, setToggle] = useState(false);
-  const [running, setRunning] = useState(false);
-  const [time, setTime] = useState(0);
 
+
+export default function StopWatch() {
+  const [time, setTime] = useState(0); // time in seconds
+  const [running, setRunning] = useState(false);
+
+  
   useEffect(() => {
-    let interval = null;
+    let interval;
     if (running) {
       interval = setInterval(() => {
         setTime((prev) => prev + 1);
       }, 1000);
-    } else {
-      clearInterval(interval);
     }
-
     return () => clearInterval(interval);
   }, [running]);
-  const handleClick = () => {
-    setToggle((prev) => {
-      const newToggle = !prev;
-      setRunning(newToggle);
-      return newToggle;
-    });
-  };
 
   const formatTime = () => {
     const minutes = Math.floor(time / 60);
     const seconds = time % 60;
-   
-    return `${minutes}.${seconds < 10 ? "0" : ""}${seconds}`;
+    return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+  };
+
+  const handleStartStop = () => setRunning((prev) => !prev);
+
+  const handleReset = () => {
+    setTime(0);
+    setRunning(false);
   };
 
   return (
-    <div>
+    <div >
       <h1>Stopwatch</h1>
-      <p>{formatTime()}</p>
-      <button onClick={handleClick}>{!toggle ? "Start" : "Stop"}</button>
-      <button onClick={()=>{setTime(0)}}>Reset</button>
+      <p>Time: {formatTime()}</p>
+      <button onClick={handleStartStop}>{running ? "Stop" : "Start"}</button>
+      <button onClick={handleReset} >
+        Reset
+      </button>
     </div>
   );
 }
